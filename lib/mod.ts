2d052c9320fd6ed79208dd449c457654a8ee1b54
UrielCh <uriel.chemouni@gmail.com>
2023-05-30 17:07:51 +0300

    drop old generation

--- lib/mod.ts
+++
@@ -1,8 +0,0 @@
-/// <reference no-default-lib="true" />
-/// <reference lib="dom" />
-/// <reference lib="dom.iterable" />
-/// <reference lib="dom.asynciterable" />
-/// <reference lib="deno.ns" />
-
-export * from "./iconBase.tsx";
-export * from "./iconContext.ts";
